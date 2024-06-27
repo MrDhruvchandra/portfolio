d8530a060159be3e675ab68b7e70150dcd6e1c47
MrDhruvchandra <dhruvmaurya298@gmail.com>
2024-06-27 18:40:28 +0530

    new

---
+++ mr.dhruva_portfolio/r.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#multiple-text', {
+    strings: [' a Frontent Web  Developer.', 'an ECE Engineer.', 'a Freelancer.'],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+ 
